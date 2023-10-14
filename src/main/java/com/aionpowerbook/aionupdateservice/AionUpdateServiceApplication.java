6c3caf0a1d879f548c9aa18c1dd0cf3ddbea4b90
Kelekelio <cristal01@gmail.com>
2023-10-14 22:30:10 +0200

    Initial commit

---
+++ src/main/java/com/aionpowerbook/aionupdateservice/AionUpdateServiceApplication.java
@@ -0,0 +1,13 @@
+package com.aionpowerbook.aionupdateservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AionUpdateServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AionUpdateServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/aionpowerbook/aionupdateservice/AionUpdateServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aionpowerbook.aionupdateservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AionUpdateServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
